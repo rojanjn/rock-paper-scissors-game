4a4c037212ccf0d8ee3dc98282de5843e7bf831c
Rojan Jafarnezhad <101561560@georgebrown.ca>
2025-05-22 13:44:06 +0330

    added logic

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -8,10 +8,6 @@
             //the user plays against the machine
 
             string[] options = { "Rock", "Paper", "Scissors" };
-            Random random = new Random();
-            string randomChoice = options[random.Next(options.Length)];
-
-            int counter = 0;
 
             //introduction & instructions
             Console.WriteLine();
@@ -49,6 +45,7 @@
                 Console.WriteLine("1 --> Rock");
                 Console.WriteLine("2 --> Paper");
                 Console.WriteLine("3 --> Scissors");
+                Console.WriteLine("4 --> Exit the game");
                 Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
                 Console.WriteLine("Please choose one:");
 
@@ -56,17 +53,105 @@
                 int userInput;
 
                 //validation
-                if (!int.TryParse(input, out userInput) || userInput < 1 || userInput > 3)
+                if (!int.TryParse(input, out userInput) || userInput < 1 || userInput > 4)
                 {
-                    Console.ForegroundColor= ConsoleColor.Cyan;
-                    Console.WriteLine("INVALID INPUT! Please enter 1, 2, or 3.");
-                    Console.ForegroundColor= ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("INVALID INPUT! Please enter 1, 2, 3, or 4.");
+                    Console.ForegroundColor = ConsoleColor.White;
                     continue; //restarting the loop
                 }
+                if (userInput == 4)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Thanks for playing!");
+                    Console.WriteLine("Take Care now..");
+                    Console.WriteLine("BYEEEEE");
+                    Console.WriteLine();
 
+                    break;
+                }
+
+                //user input to string
+                string[] choices = { "Rock", "Paper", "Scissors" };
+                string playerChoice = choices[userInput - 1];
+
+                //computer's turn
+                Random random = new Random();
+                string compChoice = choices[random.Next(choices.Length)];
+
+                Console.WriteLine("----------==========---------");
+                Console.WriteLine();
+                Console.WriteLine($"{player}, you chose {playerChoice}");
+                Console.WriteLine($"Computer chose {compChoice}");
+                Console.WriteLine();
+                Console.WriteLine("----------==========---------");
+
+                //determining losses
+                if (playerChoice == "Rock" && compChoice == "Paper")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("YOU LOST!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Better luck next time ;)");
+                    Console.WriteLine();
+                }
+
+                if (playerChoice == "Scissors" && compChoice == "Rock")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("YOU LOST!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Better luck next time ;)");
+                    Console.WriteLine();
+                }
+
+                if (playerChoice == "Paper" && compChoice == "Scissors")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("YOU LOST!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Better luck next time ;)");
+                    Console.WriteLine();
+                }
+
+                //determining wins
+                if (playerChoice == "Rock" && compChoice == "Scissors")
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("YOU WON!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Congrats ;)");
+                    Console.WriteLine();
+                }
+
+                if (playerChoice == "Paper" && compChoice == "Rock")
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("YOU WON!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Congrats ;)");
+                    Console.WriteLine();
+                }
+
+                if (playerChoice == "Scissors" && compChoice == "Paper")
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("YOU WON!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Congrats ;)");
+                    Console.WriteLine();
+                }
+
+                //determining draw
+                if (playerChoice == compChoice)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("OMG that's a DRAW!");
+                    Console.WriteLine();
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
 
             }
-
         }
     }
 }
