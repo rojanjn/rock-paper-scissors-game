2477b3a6bb8a257d8d6f1cad60a606bc603d3ff5
Rojan Jafarnezhad <101561560@georgebrown.ca>
2025-05-22 12:42:17 +0330

    game menu added

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
+            //rock paper scissors game
+            //the user plays against the machine
+
+            Random random = new Random();
+            //string randomChoice = random("Rock", "Paper", "Scissors");
+
+            int counter = 0;
+
+            Console.WriteLine("");
+            Console.WriteLine("WELCOME TO THE GAME!");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("In this game, you will have to choose between Rock, Paper, or Scissors.");
+            Console.WriteLine("And the machine will choose one randomly as well.");
+            Console.WriteLine("Let's see who wins the most rounds :)");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Before starting the game, please let me know your name:");
+            string player = Console.ReadLine();
+
+            Console.WriteLine("Pretty name!");
+            Console.WriteLine("");
+            Console.WriteLine("Now, LET'S PLAY!");
+            Console.WriteLine("");
+
             
+
         }
     }
 }
